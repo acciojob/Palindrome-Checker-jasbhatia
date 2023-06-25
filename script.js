0883edaac32ce8b1b6ed891a752a98ebe5c99b60
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-06-25 17:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-let arr1=str.split(" ");
-	console.log(arr1);
+let arr1 = str.split(" ");
+
+let arr2 = arr1.join("");
+let arr3 = arr2.split("");
+// console.log(arr3);
+// let arr2 = arr1.reverse();
+let arr4 = arr3.reverse();
+
+// console.log(arr4);
+if (arr3 == arr4) {
+  return true;
+} else {
+  return false;
+}
 }
 module.exports = palindrome
